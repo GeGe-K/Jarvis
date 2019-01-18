35cfcc637f1ac4b9b3c8a282d71a369287b6a097
GeGe-K <gloriagivondo@gmail.com>
2019-01-18 04:41:46 +0300

    initialized the api-ai client

--- src/app/chat/chat.service.ts
+++ src/app/chat/chat.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { ApiAiClient } from 'api-ai-javascript';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
+    readonly token = environment.dialogflow.Client_ID;
+    readonly client = new ApiAiClient({accessToken: this.token});
 
   constructor() { }
+
 }
