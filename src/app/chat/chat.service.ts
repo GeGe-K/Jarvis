dc8a14b42e606322fe3e3714e06d780c8e76efb6
GeGe-K <gloriagivondo@gmail.com>
2019-01-18 07:52:09 +0300

    distinguished betweeen bot and human message

--- src/app/chat/chat.service.ts
+++ src/app/chat/chat.service.ts
@@ -15,13 +15,27 @@ export class Message {
   providedIn: 'root'
 })
 export class ChatService {
-    readonly token = environment.dialogflow.Client_ID;
-    readonly client = new ApiAiClient({accessToken: this.token});
+  readonly token = environment.dialogflow.Client_ID;
+  readonly client = new ApiAiClient({accessToken: this.token});
+  conversation = new BehaviorSubject<Message[]>([]);
 
   constructor() { }
 
-  talk() {
-    this.client.textRequest('Who are you?')
-      .then (res => console.log(res));
+  // Adds message to source
+  update(msg: Message) {
+    this.conversation.next[(msg)];
   }
+
+  // Sends and receives messages via DialogFlow
+  converse(msg: string) {
+    const userMessage = new Message(msg, 'user');
+    this.update(userMessage);
+
+    return this.client.textRequest(msg).then(res => {
+      const speech = res.result.fulfillment.speech;
+      const botMessage = new Message(speech, 'bot');
+      this.update(botMessage);
+    });
+  }
+
 }
