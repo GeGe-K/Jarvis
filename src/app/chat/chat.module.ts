5190985e242c85f8d1f0a40f3189542babb36b77
GeGe-K <gloriagivondo@gmail.com>
2019-01-18 03:59:13 +0300

    created a chat-dialog component

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChatDialogComponent } from './chat-dialog/chat-dialog.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [ChatDialogComponent]
 })
 export class ChatModule { }
